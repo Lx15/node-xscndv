e4e833f9e8e43343e88cfcae3e9d3ea65c83eafd
Lx15 <lx2011xd@163.com>
2021-06-26 23:03:08 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# node-xscndv
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/node-xscndv)
---
+++ index.js
@@ -0,0 +1,6 @@
+// run `node index.js` in the terminal
+
+console.log(`Hello Node.js v${process.versions.node}!`);
+function blue(str) {
+  return '\x1b[1m\x1b[34m' + str + '\x1b[39m\x1b[22m';
+}
